14c95d0acc5f4c94c19c251d8cd5408cfada127b
Renan89 <leal89renan@outlook.com>
2024-12-13 10:51:43 -0300

    testes

--- app/src/test/java/org/example/service/MatriculaServiceTest.java
+++ app/src/test/java/org/example/service/MatriculaServiceTest.java
@@ -1,15 +1,15 @@
 package org.example.service;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.assertThat;
 
+import org.example.entity.Turma;
 import org.example.exception.NotFoundException;
 import org.example.repository.AlunoRepository;
 import org.example.repository.AlunoRepositoryFake;
 import org.example.repository.TurmaRepository;
 import org.example.repository.TurmaRepositoryFake;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 
 public class MatriculaServiceTest {
 
@@ -51,6 +51,49 @@ public class MatriculaServiceTest {
     assertThatThrownBy(() -> matriculaService.matricular(20221111, "tda-2024-2"))
         .isInstanceOf(NotFoundException.class)
         .hasMessage("Turma tda-2024-2 não encontrada");
+  }
+
+  @Test
+  void TestMatricularAlunoEmTurmaExistente() {
+    matriculaService.matricular(20221111, "tds-2024-2");
+    // Simula a chamada para verificar se a turma foi atualizada corretamente após a
+    // matrícula.
+    Turma turma = turmaRepository.findByCodigo("tds-2024-2").orElseThrow();
+    assertThat(turma.getVagas()).isEqualTo(9);
+  }
+
+  @Test
+  void TestMatricularAlunoEmTurmaComVagasEsgotadas() {
+    Turma turma = turmaRepository.findByCodigo("tds-2024-2").orElseThrow();
+    turma.setVagas(0);
+
+    assertThatThrownBy(() -> matriculaService.matricular(20221111, "tds-2024-2"))
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessage("Turma tds-2024-2 não possui mais vagas disponíveis");
+  }
+
+  @Test
+  void TestMatricularAlunoDuasVezesNaMesmaTurma() {
+    matriculaService.matricular(20221111, "tds-2024-2");
+    assertThatThrownBy(() -> matriculaService.matricular(20221111, "tds-2024-2"))
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessage("Aluno 20221111 já está matriculado na turma tds-2024-2");
+  }
+
+  @Test
+  void TestMatricularAlunoComDadosInvalidos() {
+    assertThatThrownBy(() -> matriculaService.matricular(-1, "invalid-turma"))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Dados da matrícula são inválidos");
+  }
+
+  @Test
+  void TestMatricularAlunoEmTurmaComVagasAtualizacao() {
+    Turma turma = turmaRepository.findByCodigo("tds-2024-2").orElseThrow();
+    turma.setVagas(5);
+
+    matriculaService.matricular(20221111, "tds-2024-2");
+    assertThat(turma.getVagas()).isEqualTo(4);
   }
 
 }
